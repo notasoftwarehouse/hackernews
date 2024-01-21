2e1df277a03ac70fb3b6dd2b2f19ba7fc41db47e
zayminmaw <zayminmaw77@gmail.com>
2024-01-21 20:42:56 +0630

    hotfix: site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hackernews.press",
+  site: "https://www.hackernews.press",
   integrations: [
     react(),
     tailwind({
